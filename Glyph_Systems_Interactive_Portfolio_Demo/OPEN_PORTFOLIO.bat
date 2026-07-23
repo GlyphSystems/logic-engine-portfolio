bb194ddc9edb7aa156c80e10b9880d7ae053890c
Eugene Allen <eugenerallen@gmail.com>
2026-07-23 11:59:31 -0700

    Delete Glyph_Systems_Interactive_Portfolio_Demo directory

--- Glyph_Systems_Interactive_Portfolio_Demo/OPEN_PORTFOLIO.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-set PORT=8765
-start "Glyph Systems Portfolio Server" /min py -m http.server %PORT% --bind 127.0.0.1
-ping 127.0.0.1 -n 3 >nul
-start "" "http://127.0.0.1:%PORT%/index.html"
-exit /b
